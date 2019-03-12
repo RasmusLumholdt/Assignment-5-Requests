7a2927f00dd0156335af5229f90c1c110444a062
RasmusLumholdt <dev.rlumholdt@gmail.com>
2019-03-12 20:36:03 +0100

    Fixed some things

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     
     
-    if(request.get_json()['password'] == 'ab'):
+    if(request.get_json()['password'] == 'az'):
         return Response('{"Message": "Logged in"}', status=200, mimetype='application/json')
     else:
         return Response('{"Message": "Failed"}', status=403, mimetype='application/json')
--- server2.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, Response, request
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    pass = request.get_json()
